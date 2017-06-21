63688735dc6e1d8dd6f4a695bdab616da3f318a6
getsanjeev <getsanjeevdubey@gmail.com>
2017-06-21 12:02:15 +0530

    added noise  to images

--- bp.py
+++ bp.py
@@ -4,10 +4,32 @@ import os
 import csv
 
 
+
+def add_salt_and_pepper_noise(image):
+	s_p_ratio = 0.5
+	quantity = 0.005
+	noisy_image = image.copy()
+	num_salt_points = np.ceil(image.size*quantity*s_p_ratio)
+	random_salt_coordinates_x = np.random.randint(image.shape[0],size = int(num_salt_points))
+	random_salt_coordinates_y = np.random.randint(image.shape[1],size = int(num_salt_points))
+	for i, j in zip(random_salt_coordinates_x, random_salt_coordinates_y):
+		noisy_image[i,j] = 0
+
+	num_pepper_points = np.ceil(image.size*quantity*(1-s_p_ratio))
+	random_pepper_coordinates_x = np.random.randint(image.shape[0],size = int(num_pepper_points))
+	random_pepper_coordinates_y = np.random.randint(image.shape[1],size = int(num_pepper_points))
+	for i, j in zip(random_pepper_coordinates_x, random_pepper_coordinates_y):
+		noisy_image[i,j] = 255
+	return noisy_image
+
+
+
+
+
 if __name__ == "__main__":
-    pathFolder = "/home/sherlock/Internship@iit/exudate-detection/Base11/"
+    pathFolder = "/home/sherlock/Internship@iit/exudate-detection/diaretdb1/"
     filesArray = [x for x in os.listdir(pathFolder) if os.path.isfile(os.path.join(pathFolder,x))]
-    destinationFolder = "/home/sherlock/Internship@iit/exudate-detection/Base11-exudates/"
+    destinationFolder = "/home/sherlock/Internship@iit/exudate-detection/diaretdb1-exudates/"
 
     if not os.path.exists(destinationFolder):
         os.mkdir(destinationFolder)
@@ -16,8 +38,10 @@ if __name__ == "__main__":
     	print(pathFolder+'/'+file_name)
     	file_name_no_extension = os.path.splitext(file_name)[0]
     	fundus = cv2.imread(pathFolder+'/'+file_name)
-    	b,green_fundus,r = cv2.split(fundus)
-    	cv2.imwrite(destinationFolder+file_name_no_extension+"_exudates.jpg",green_fundus)	
+    	hsi = cv2.cvtColor(fundus, cv2.COLOR_BGR2HSV)
+    	h,s,v = cv2.split(hsi)
+    	noisy_image = add_salt_and_pepper_noise(v)
+    	cv2.imwrite(destinationFolder+file_name_no_extension+"_exudates.jpg",noisy_image)	
 
 
 
