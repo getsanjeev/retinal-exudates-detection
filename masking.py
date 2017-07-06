527f1b02aad7f5d712ad50ee559e69b528c862a3
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-06 23:50:05 +0530

    added cropping function

--- masking.py
+++ masking.py
@@ -2,18 +2,24 @@ import cv2
 import numpy as np
 import os
 import csv
+import math
 import time
-from numba.decorators import jit, autojit
+from numba import jit
 
+@jit
 def crop_circle(image, radius, centerX, centerY):
 	recX = 2*centerX
 	recY = 2*centerY
 	cropped_image = np.zeros((recX,recY), dtype="uint8") * 255
+	image[centerX,centerY] = 0
+	image[centerX-1,centerY] = 0	
+	image[centerX-2,centerY] = 0
+	image[centerX-3,centerY] = 0
 	start_time = time.time()
 	for i in range(image.shape[0]):
 		for j in range(image.shape[1]):
-			if(i ==j):
-				image[i,j] = 0
+			if int(math.pow((i - centerX),2) + math.pow((j - centerY),2)) < math.pow(radius,2):
+				image[i,j] = 0				
 	print("--- %s seconds ---" % (time.time() - start_time))
 	return image
 
@@ -25,10 +31,10 @@ image_mask = cv2.imread("sss.tif");
 b,green_fundus,r = cv2.split(image_mask)
 dim = (800,615)
 fundus1 = cv2.resize(green_fundus, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow("dfddfsse",fundus1)
 centerX = 400
 centerY = 307
 radius = 290
-#pairwise_numba = autojit(crop_circle)
 qimage = crop_circle(fundus1,radius,centerY,centerY)
 cv2.imshow("dfddfe",qimage)
 cv2.waitKey()
