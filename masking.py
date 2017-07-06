4c7bdb88f478b875caf6c5ab48b9adfdf9ff54f9
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-06 22:59:35 +0530

    added numba jit

--- masking.py
+++ masking.py
@@ -2,14 +2,20 @@ import cv2
 import numpy as np
 import os
 import csv
+import time
+from numba.decorators import jit, autojit
 
 def crop_circle(image, radius, centerX, centerY):
 	recX = 2*centerX
 	recY = 2*centerY
 	cropped_image = np.zeros((recX,recY), dtype="uint8") * 255
-	if (abs(centerX-image) <= radius).nonzero() and (abs(centerY-image) <= radius).nonzero():
-		cropped_image = image
-	cv2.imwrite("cropp.jpg",cropped_image)	
+	start_time = time.time()
+	for i in range(image.shape[0]):
+		for j in range(image.shape[1]):
+			if(i ==j):
+				image[i,j] = 0
+	print("--- %s seconds ---" % (time.time() - start_time))
+	return image
 
 
 
@@ -22,5 +28,8 @@ fundus1 = cv2.resize(green_fundus, dim, interpolation = cv2.INTER_AREA)
 centerX = 400
 centerY = 307
 radius = 290
-crop_circle(fundus1,radius,centerY,centerY)
+#pairwise_numba = autojit(crop_circle)
+qimage = crop_circle(fundus1,radius,centerY,centerY)
+cv2.imshow("dfddfe",qimage)
+cv2.waitKey()
 
