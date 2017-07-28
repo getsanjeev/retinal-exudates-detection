d5c021f560c9af56c900eba53600707cfcf97f7a
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-28 11:24:41 +0530

    added entropy feature

--- k_means/cluster.py
+++ k_means/cluster.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 from numba import jit
+import os
 from matplotlib import pyplot as plt
+import math
 
+@jit
 def standard_deviation_image(image):
 	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 	clahe_output = clahe.apply(image)
@@ -19,6 +22,86 @@ def standard_deviation_image(image):
 		i = i+15
 	return result
 
+def line_of_symmetry(image):
+	image_v = image.copy()
+	line = 0
+	prev_diff = image_v.size
+	for i in range(20,image_v.shape[0]-20):
+		x1, y1 = image_v[0:i,:].nonzero()
+		x2, y2 = image_v[i+1:image_v.shape[0],:].nonzero()
+		diff = abs(x1.shape[0] - x2.shape[0])
+		if diff < prev_diff:
+			prev_diff = diff
+			line = i
+		i = i + 35
+	return line
+
+def identify_OD(image):
+	newfin = cv2.dilate(image, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations=2)
+	mask = np.ones(newfin.shape[:2], dtype="uint8") * 255
+	y1, ycontours, yhierarchy = cv2.findContours(newfin.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	prev_contour = ycontours[0]
+	for cnt in ycontours:
+		if cv2.contourArea(cnt) >= cv2.contourArea(prev_contour):
+			prev_contour = cnt
+			cv2.drawContours(mask, [cnt], -1, 0, -1)
+	M = cv2.moments(prev_contour)
+	cx = int(M['m10']/M['m00'])
+	cy = int(M['m01']/M['m00'])
+	print(cx,cy)
+	return (cx,cy)
+
+def identify_OD_bv_density(blood_vessel_image):
+	los = line_of_symmetry(blood_vessel_image)
+	sub_image = blood_vessel_image[los-100:los+100,:]	
+	i = 0
+	index = 0
+	density = -1
+	rr = 0	
+	while i < sub_image.shape[1]:
+		x1,y1 = sub_image[:,i:i+50].nonzero()
+		count = x1.shape[0]		
+		if(density < count):
+			density = count
+			index = i
+		i = i + 30
+	cv2.circle
+	return (cx,cy)
+
+
+def generate_csv(hue_image, intensity_image, SD_image, edge_image):
+	with open('kmeans_featues.csv', 'w') as csvfile:
+		filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		filewriter.writerow(['hue','intensity','standard_deviation','edges'])
+	while i < hue_image.shape[0]:
+		j = 0
+		while j < hue_image.shape[1]:
+			filewriter.writerow([hue_image[i,j],intensity_image[i,j],SD_image[i,j],edge_image[i,j]])
+			j = j + 1
+		i = i + 1
+
+@jit
+def calculate_entropy(image):
+	entropy = image.copy()
+	sum = 0
+	i = 0
+	j = 0
+	while i < entropy.shape[0]:
+		j = 0
+		while j < entropy.shape[1]:
+			sub_image = entropy[i:i+10,j:j+10]
+			histogram = cv2.calcHist([sub_image],[0],None,[256],[0,256])
+			sum = 0
+			for k in range(256):
+				if histogram[k] != 0:					
+					sum = sum + (histogram[k] * math.log(histogram[k]))
+				k = k + 1
+			entropy[i:i+10,j:j+10] = sum
+			j = j+9
+		i = i+9
+	ret2,th2 = cv2.threshold(entropy,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	return th2
+
 @jit
 def edge_pixel_image(image,bv_image):
 	edge_result = image.copy()
@@ -32,7 +115,7 @@ def edge_pixel_image(image,bv_image):
 				edge_result[i,j] = 0
 			j = j+1
 		i = i+1
-	newfin = cv2.dilate(edge_result, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations=1)	
+	newfin = cv2.dilate(edge_result, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)), iterations=1)
 	return newfin
 
 def extract_bv(image):			
@@ -82,22 +165,34 @@ def extract_bv(image):
 	blood_vessels_1 = cv2.bitwise_not(dilated)
 	return blood_vessels_1
 
-fundus = cv2.imread("20051020_64007_0100_PP.tif")
-print(fundus.shape)
-hsv_fundus = cv2.cvtColor(fundus,cv2.COLOR_BGR2HSV)
-h,s,v = cv2.split(hsv_fundus)
-cv2.imwrite("hue.jpg",h)
-gray_scale = cv2.cvtColor(fundus,cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray.jpg",gray_scale)
-bv_image = extract_bv(gray_scale)
-cv2.imwrite("bv.jpg",bv_image)
-var_fundus = standard_deviation_image(gray_scale)
-cv2.imwrite("var.jpg",var_fundus)
-edge_candidates = edge_pixel_image(gray_scale,bv_image)
-cv2.imwrite("edge.jpg",edge_candidates)
-print(hsv_fundus.shape)
 
-cv2.waitKey()
+if __name__ == "__main__":
+	pathFolder = "/home/sherlock/Internship@iit/exudate-detection/Base11/"
+	filesArray = [x for x in os.listdir(pathFolder) if os.path.isfile(os.path.join(pathFolder,x))]
+	DestinationFolder = "/home/sherlock/Internship@iit/exudate-detection/Base11-exudates-kmeans/"
+	
+	if not os.path.exists(DestinationFolder):
+		os.mkdir(DestinationFolder)
+	# with open('hemmorages12.csv', 'w') as csvfile:
+	# 	filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	# 	filewriter.writerow(['image_name', 'hue','intensity','standard_deviation','edges'])
+	for file_name in filesArray:
+		print(pathFolder+'/'+file_name)
+		file_name_no_extension = os.path.splitext(file_name)[0]
+		fundus = cv2.imread(pathFolder+'/'+file_name)				
+		hsv_fundus = cv2.cvtColor(fundus,cv2.COLOR_BGR2HSV)
+		h,s,v = cv2.split(hsv_fundus)	
+		gray_scale = cv2.cvtColor(fundus,cv2.COLOR_BGR2GRAY)
+		clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+		contrast_enhanced_fundus = clahe.apply(gray_scale)
+		entropy = calculate_entropy(contrast_enhanced_fundus)								
+		bv_image = extract_bv(gray_scale)
+		#los = line_of_symmetry(bv_image)	
+		var_fundus = standard_deviation_image(gray_scale)
+		edge_candidates = edge_pixel_image(gray_scale,bv_image)
+		# (cx1,cy1) = identify_OD(gray_scale)
+		# (cx2,cy2) = identify_OD_bv_density(bv_image)
+		cv2.imwrite(DestinationFolder+file_name_no_extension+"_exudates_kmeans.jpg",entropy)		
 
 # X = np.random.randint(25,50,(25,2))
 # Y = np.random.randint(60,85,(25,2))
